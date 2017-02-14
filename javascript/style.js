49648e3223ece303165a3ee6261cb40950101f61
Alexandru <axentioialexandru95@gmail.com>
2017-02-14 18:04:50 +0200

    Reworked and added About Page

---
+++ javascript/style.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+AOS.init();
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+        $(".navbar").addClass('big');
+    }
+    else {
+        $(".navbar").removeClass('big');
+    }
+});
+
+$('.slider').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+$('#overlay').click(function() {
+	$('.navbar-overlay').fadeIn(400)
+});
+
+$('#overlay').click(function() {
+	$('.navbar-toggle').fadeOut(10)
+});
+
+
+$('#close').click(function() {
+	$('.navbar-overlay').fadeOut(400)
+});
+
+$('#close').click(function() {
+	$('.navbar-toggle').fadeIn(400)
+});
+
+
+});
